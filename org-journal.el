bastibe/org-journal

commit 9cb0ace698026a198044c2ff617636a57d4792fa

corrected file pattern to match whole file name
%Y%m%d will not match .#20140806 any more.

org-journal.el
@@ -2,7 +2,7 @@
 
 ;; Author: Bastian Bechtold
 ;; URL: http://github.com/bastibe/emacs-journal
-;; Version: 1.5.1
+;; Version: 1.5.2
 
 ;; Adapted from http://www.emacswiki.org/PersonalDiary
 
@@ -59,13 +59,14 @@
 
 (defvar org-journal-date-list nil)
 (defvar org-journal-file-pattern
-  "\\(?1:[0-9]\\{4\\}\\)\\(?2:[0-9][0-9]\\)\\(?3:[0-9][0-9]\\)$")
+  "^\\(?1:[0-9]\\{4\\}\\)\\(?2:[0-9][0-9]\\)\\(?3:[0-9][0-9]\\)$")
 
 ;;;###autoload
 (defun org-journal-format-string->regex (format-string)
   "Update org-journal-file-pattern with the current
   org-journal-file-format"
   (concat
+   "^"
    (replace-regexp-in-string
     "%d" "\\\\(?3:[0-9][0-9]\\\\)"
     (replace-regexp-in-string

commit d54a66f9aeda7f422dd477f18779c98c5a228b28

fixes auto-mode-alist entry
this regression was introduced in 9cb0ace698026a198044c2ff617636a57d4792fa

org-journal.el
@@ -2,7 +2,7 @@
 
 ;; Author: Bastian Bechtold
 ;; URL: http://github.com/bastibe/emacs-journal
-;; Version: 1.5.2
+;; Version: 1.5.3
 
 ;; Adapted from http://www.emacswiki.org/PersonalDiary
 
@@ -50,7 +50,7 @@
   "Update auto-mode-alist to open journal files in
   org-journal-mode"
   (let ((name (concat (file-truename org-journal-dir)
-                      org-journal-file-pattern)))
+                      (substring org-journal-file-pattern 1))))
     (add-to-list 'auto-mode-alist
                  (cons name 'org-journal-mode))))
 