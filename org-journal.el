bastibe/org-journal

commit b0f3d326323c491aebcdd0465952e49d33f0346d

Fix carryover problem
When previous journal is encrypted, and `org-journal-encrypt-journal` is
`nil`, it did not return the gpg file with the .gpg extension.

org-journal.el
@@ -276,13 +276,13 @@ Otherwise, date ascending."
 (defun org-journal-get-entry-path (&optional time)
   "Return the path to an entry given a TIME.
 If no TIME is given, uses the current time."
-  (file-truename
-   (expand-file-name
-    (format-time-string
-     (concat org-journal-file-format
-             (if org-journal-encrypt-journal ".gpg" ""))
-     time)
-    (file-name-as-directory org-journal-dir))))
+  (let ((file (file-truename
+               (expand-file-name
+                (format-time-string (concat org-journal-file-format) time)
+                (file-name-as-directory org-journal-dir)))))
+    (unless (file-exists-p file)
+      (setq file (concat file ".gpg")))
+    file))
 
 (defun org-journal-dir-check-or-create ()
   "Check existence of `org-journal-dir'. If it doesn't exist, try to make directory."

commit f4bb031de1324c9b11804f8f641e5a83d4c84651

Fix #138
This fixes a bug introduced in commit b0f3d326323c491aebcdd0465952e49d33f0346d.

org-journal.el
@@ -280,9 +280,9 @@ Otherwise, date ascending."
 If no TIME is given, uses the current time."
   (let ((file (file-truename
                (expand-file-name
-                (format-time-string (concat org-journal-file-format) time)
+                (format-time-string org-journal-file-format time)
                 (file-name-as-directory org-journal-dir)))))
-    (unless (file-exists-p file)
+    (when (and org-journal-encrypt-journal (not (file-exists-p file)))
       (setq file (concat file ".gpg")))
     file))
 
@@ -545,7 +545,9 @@ If the date is in the future, create a schedule entry."
   (let ((file-list (directory-files-recursively
                     (file-truename (expand-file-name (file-name-as-directory org-journal-dir))) "\.*"))
         (predicate (lambda (file-path)
-                     (string-match-p org-journal-file-pattern (file-truename file-path)))))
+                     (and (string-match-p org-journal-file-pattern (file-truename file-path))
+                          (or org-journal-encrypt-journal
+                              (not (string-match-p "\.gpg$" (file-truename file-path))))))))
     (seq-filter predicate file-list)))
 
 (defun org-journal-list-dates ()