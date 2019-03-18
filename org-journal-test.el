86833edc354aefc319c6fd58d999c1460418ebab
Christian Schwarzgruber <c.schwarzgruber.cs@gmail.com>
2019-03-18 12:17:03 +0100

    Add carryover test for #146

--- org-journal-test.el
+++ org-journal-test.el
@@ -6,6 +6,26 @@
 ;;
 ;; Description:
 ;;
+;; Test org-journal in a clean Emacs environment. You need to adopt the
+;; path to the package `use-package', `bind-key' and `org-journal'.
+;;
+;; (add-to-list 'load-path "~/.emacs.d/elpa/use-package-20181119.2350")
+;; (add-to-list 'load-path "~/.emacs.d/elpa/bind-key-20180513.430")
+;; (require 'use-package)
+;;
+;; (use-package org-journal
+;;   :load-path "~/Developing/third_party/org-journal"
+;;   :defer 0.5
+;;   :bind (("<f9> s" . org-journal-search)
+;;          ("<f9> j" . org-journal-new-entry))
+;;   :init
+;;   ;; Must be set before loading org-journal
+;;   (setq org-journal-dir "~/emacs/org/journal/")
+;;   :config
+;;   (setq org-journal-encrypt-journal t
+;;         org-journal-file-type 'yearly))
+;;
+
 (require 'org-journal)
 
 (defvar org-journal-dir-test "/tmp/org-journal")
@@ -50,24 +70,35 @@
                    (encode-time 0 0 0 1 1 2019)))))
 
 
-(ert-deftest org-journal-new-entry-test ()
+(ert-deftest org-journal-carryover-items-test ()
   "Org journal new enty test."
   (org-journal-dir-test-setup)
   (with-temp-buffer
     (insert "* Tuesday, 01/01/19\n")
     (org-set-property "CREATED" "20190101")
+    (insert "** 13:00 Some journal entry\n")
     (insert "* Wednesday, 01/02/19\n")
     (org-set-property "CREATED" "20190102")
+    (insert "** TODO First\n")
+    (insert "** 13:00 Some journal entry 1\n")
+    (insert "** TODO Second\n")
+    (insert "** 14:00 Some journal entry 2\n")
     (write-file (expand-file-name "20181231" org-journal-dir-test)))
-  (let ((org-journal-dir org-journal-dir-test)
-        (org-journal-file-type 'weekly)
-        (org-journal-carryover-items nil)
-        (org-journal-enable-encryption nil))
-    (org-journal-new-entry nil)
-    (goto-char (point-max))
-    (insert "\n*** TODO TEST")
-    (save-buffer)
+  (let* ((org-journal-dir org-journal-dir-test)
+         (comment-start-skip "^\\s-*#\\(?: \\|$\\)")
+         (org-journal-file-pattern (org-journal-file-pattern-test))
+         (org-journal-file-type 'weekly)
+         (org-journal-carryover-items "TODO=\"TODO\"")
+         (org-journal-encrypt-journal nil)
+         (org-journal-enable-encryption nil)
+         (org-journal-date-format "Test header"))
+    (org-journal-new-entry t)
+    (kill-buffer "20181231")
+    (write-file (org-journal-get-entry-path))
     (kill-buffer)
-    (message "%s" (with-temp-buffer
-                    (insert-file-contents (org-journal-get-entry-path))
-                    (buffer-substring-no-properties (point-min) (point-max))))))
+    (should (string= (with-temp-buffer
+                       (insert-file-contents (org-journal-get-entry-path))
+                       (buffer-substring-no-properties (point-min) (point-max)))
+                     (concat "* Test header\n:PROPERTIES:\n:CREATED:  "
+                             (format-time-string "%Y%m%d")
+                             "\n:END:\n** TODO First\n** TODO Second\n")))))
