bastibe/org-journal

commit 2c43b10eed0659f8e47797e5e53d2973f939284d

Only get CREATED property for level 1 headings (Fix #231)
Journal entries are always level 1 headings, so use that as an anchor.

org-journal.el
@@ -946,7 +946,8 @@ This is the counterpart of `org-journal-file-name->calendar-date' for
      (save-excursion
        (goto-char (point-min))
        (while (re-search-forward org-journal-created-re nil t)
-         (push (org-journal-entry-date->calendar-date) dates))
+         (when (= (save-excursion (org-back-to-heading) (outline-level)) 1)
+           (push (org-journal-entry-date->calendar-date) dates)))
        dates))))
 
 ;;;###autoload
@@ -1612,7 +1613,8 @@ If STR is empty, search for all entries using `org-journal-time-prefix'."
                                    (org-journal-file-name->calendar-date fname)
                                  (save-excursion
                                    (when (re-search-backward org-journal-created-re nil t)
-                                     (org-journal-entry-date->calendar-date))))))
+                                     (when (= (save-excursion (org-back-to-heading) (outline-level)) 1)
+                                       (org-journal-entry-date->calendar-date)))))))
                           (when date
                             (org-journal-calendar-date->time date)))
                         (- (point) (length str))

commit 524009b5daa9ab4740d1c8d91857eb89a6aff07c

Fix regression (Fix #233)
The files are opened in fundamental mode and hence not all functions are
working correctly.

The error was introduced in 2c43b10.

org-journal.el
@@ -715,8 +715,10 @@ hook is run."
                             ;; “time” is on some other day, use blank timestamp
                             (t ""))))
           (insert org-journal-time-prefix timestamp))
+
         (unless (null org-journal-skip-carryover-drawers)
           (org-journal-remove-drawer))
+
         (run-hooks 'org-journal-after-entry-create-hook))
 
       (if (and org-journal-hide-entries-p (org-journal-time-entry-level))
@@ -943,9 +945,9 @@ This is the counterpart of `org-journal-file-name->calendar-date' for
    file
    (let (dates)
      (save-excursion
-       (goto-char (point-min))
+       (org-first-headline-recenter)
        (while (re-search-forward org-journal-created-re nil t)
-         (when (= (save-excursion (org-back-to-heading) (outline-level)) 1)
+         (when (= (save-excursion (org-back-to-heading) (org-outline-level)) 1)
            (push (org-journal-entry-date->calendar-date) dates)))
        dates))))
 
@@ -1612,7 +1614,7 @@ If STR is empty, search for all entries using `org-journal-time-prefix'."
                                    (org-journal-file-name->calendar-date fname)
                                  (save-excursion
                                    (when (re-search-backward org-journal-created-re nil t)
-                                     (when (= (save-excursion (org-back-to-heading) (outline-level)) 1)
+                                     (when (= (save-excursion (org-back-to-heading) (org-outline-level)) 1)
                                        (org-journal-entry-date->calendar-date)))))))
                           (when date
                             (org-journal-calendar-date->time date)))

org-journal-test.el
@@ -184,8 +184,7 @@
      (let ((inhibit-message t))
        (should
         (string= "No journal entry for this date."
-                 (org-journal-read-or-display-entry (encode-time 0 0 0 2 1 2019) 'noselect))))
-     (kill-buffer "20190101"))))
+                 (org-journal-read-or-display-entry (encode-time 0 0 0 2 1 2019) 'noselect)))))))
 
 (ert-deftest org-journal-search-build-file-list-test ()
   "Test for `org-journal-search-build-file-list'."