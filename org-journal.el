25ebb8f1c8cd8993fb75a15e2bb8144fb6f9e006
Christian Schwarzgruber <c.schwarzgruber.cs@gmail.com>
2020-08-23 17:11:42 +0200

    Patch release (2.1.1)

--- org-journal.el
+++ org-journal.el
@@ -4,7 +4,7 @@
 ;;         Christian Schwarzgruber
 
 ;; URL: http://github.com/bastibe/org-journal
-;; Version: 2.1.0
+;; Version: 2.1.1
 ;; Package-Requires: ((emacs "25.1") (org "9.1"))
 
 ;;; Commentary:
