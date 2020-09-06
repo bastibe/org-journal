68db70b1282f22b5c059d440b5dd02ef0792d0e9
Christian Schwarzgruber <c.schwarzgruber.cs@gmail.com>
2020-09-06 12:38:25 +0200

    Fix indentation

--- org-journal.el
+++ org-journal.el
@@ -1030,16 +1030,16 @@ arguments (C-u C-u) are given. In that case insert just the heading."
 
 (defun org-journal-sort-dates (dates calendar-date prev)
   "Sorts DATES to determine the order of journal entries. Can be advised\replaced by a user."
-  (let ((sorted-dates (copy-tree dates)))
+  (let ((sorted-dates (copy-tree dates)))  ;; We won't modify `dates', as it would change `org-journal--sorted-dates'
     (cl-loop
-      for date in sorted-dates
-      while (org-journal--calendar-date-compare date calendar-date)
-      count t into cnt
-      finally (if (> cnt 0)
-                ;; Insert new date into list
-                (setcdr (nthcdr (1- cnt) sorted-dates) (cons calendar-date (nthcdr cnt sorted-dates)))
-                ;; Insert new date at front
-                (setq sorted-dates (cons calendar-date sorted-dates))))
+       for date in sorted-dates
+       while (org-journal--calendar-date-compare date calendar-date)
+       count t into cnt
+       finally (if (> cnt 0)
+                   ;; Insert new date into list
+                   (setcdr (nthcdr (1- cnt) sorted-dates) (cons calendar-date (nthcdr cnt sorted-dates)))
+                 ;; Insert new date at front
+                 (setq sorted-dates (cons calendar-date sorted-dates))))
     ;; Reverse list for previous search.
     (when prev
       (setq sorted-dates (reverse sorted-dates)))
@@ -1051,13 +1051,12 @@ arguments (C-u C-u) are given. In that case insert just the heading."
 If PREV is non-nil, open previous entry instead of next.
 If NO-SELECT is non-nil, open it, but don't show it."
   (let ((calendar-date (if (org-journal--daily-p)
-                         (org-journal--file-name->calendar-date (file-truename (buffer-file-name)))
+                           (org-journal--file-name->calendar-date (file-truename (buffer-file-name)))
                          (while (org-up-heading-safe))
                          (org-journal--entry-date->calendar-date)))
-         (view-mode-p view-mode)
-         (dates (org-journal--list-dates)))
+        (view-mode-p view-mode)
+        (dates (org-journal--list-dates)))
     (unless (member calendar-date dates)
-      ;; Create copy of `org-journal--sorted-dates'
       (setq dates (org-journal-sort-dates dates calendar-date prev)))
     (while (and dates (car dates)
                 (or (if prev
